d094b11a2942bcb8eac489680e199ae53a8108fd
unknown <tanwiagrawal09@gmail.com>
2021-12-11 11:02:19 +0545

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots'
+    });
+    
+    
+    
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    
+    hamberger.addEventListener('click',function(){
+        mobileNav.classList.add('open');
+    });
+    times.addEventListener('click',function(){
+        mobileNav.classList.remove('open');
+    });
+    
+    });
